6d9d2f6b8890f9ee5205fb19e1d491fe53719fed
Nitin Dhemiwalm <dhemiwal.nitin@gmail.com>
2020-09-14 01:35:38 +0530

    Updates

---
+++ precache-manifest.6728a4b5c26f935bb8204af7daa12732.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "be3632c5b9f43fc2b703ecc2913e5f62",
+    "url": "/index.html"
+  },
+  {
+    "revision": "177613f82a9f775229c2",
+    "url": "/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "78cb37b13a9a3186180b",
+    "url": "/static/css/main.ae422d52.chunk.css"
+  },
+  {
+    "revision": "177613f82a9f775229c2",
+    "url": "/static/js/2.18eede6a.chunk.js"
+  },
+  {
+    "revision": "492d7e05a4c2e81da7932422a4d31130",
+    "url": "/static/js/2.18eede6a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "78cb37b13a9a3186180b",
+    "url": "/static/js/main.395e69e8.chunk.js"
+  },
+  {
+    "revision": "260ea615de11b66ab4eb",
+    "url": "/static/js/runtime-main.8f4fa31a.js"
+  },
+  {
+    "revision": "1558ef92d5ffdd976d4f7e9cec1b983b",
+    "url": "/static/media/Profile.1558ef92.jpeg"
+  },
+  {
+    "revision": "13229353a81fcf0ea8c21fa4abbb8c58",
+    "url": "/static/media/aiony-haust-3TLl_97HNJo-unsplash.13229353.jpg"
+  },
+  {
+    "revision": "b247346e69f2278272f06bddde5c2c98",
+    "url": "/static/media/anothersample.b247346e.jpg"
+  },
+  {
+    "revision": "e4cd8a253229c567ad6fd110bcfc12a9",
+    "url": "/static/media/cargo-ship.e4cd8a25.svg"
+  },
+  {
+    "revision": "e958bf9662ba1529312c70e1ee5e9006",
+    "url": "/static/media/food-and-restaurant.e958bf96.svg"
+  },
+  {
+    "revision": "1b2585ec3167e2870ff9c85638dcf964",
+    "url": "/static/media/garment.1b2585ec.svg"
+  },
+  {
+    "revision": "910ba6aff30fda831b44542882c76889",
+    "url": "/static/media/left-arrow.910ba6af.svg"
+  },
+  {
+    "revision": "0502129ac677d278364efc728b4e1bfb",
+    "url": "/static/media/milan-popovic-kOnmHdLJTNI-unsplash.0502129a.jpg"
+  },
+  {
+    "revision": "2192c858f91d7c252f3182afb12f9613",
+    "url": "/static/media/oladimeji-odunsi-tUUmR82pq68-unsplash.2192c858.jpg"
+  },
+  {
+    "revision": "65516f1749a6b6975311c09df711aae8",
+    "url": "/static/media/right-arrow.65516f17.svg"
+  }
+]);
